b9b5d530f4a279b8ff6d96153a1c9c8210541441
Kush Gupta <Kushg827@gmail.com>
2025-08-18 00:34:20 +0530

    Added jwt create and verify logic

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,4 +1,5 @@
 const UserRepository = require('../repository/user-repository.js')
+const {JWT_KEY} = require('../config/serverConfig.js');
 
 class UserService {
     constructor() {
@@ -11,6 +12,36 @@ class UserService {
             return user;
         } catch (error) {
             console.log("Error in create in user service");
+            throw error;
+        }
+    }
+
+    async getById(userId) {
+        try {
+            const user = await this.userRepository.getById(userId);
+            return user;
+        } catch (error) {
+            console.log("Error in getById in user service");
+            throw error;
+        }
+    }
+
+    createToken(user) {
+        try {
+            const result = JWT_KEY.sign(user, JWT_KEY, {expiresIn: '1h'});
+            return result;
+        } catch (error) {
+            console.log("Error in create token in token creation");
+            throw error;
+        }
+    }
+
+    verifyToken(token) {
+        try {
+            const response = jwt.verify(token, JWT_KEY);
+            return response;
+        } catch (error) {
+            console.log("Error in verigying token in user service");
             throw error;
         }
     }
