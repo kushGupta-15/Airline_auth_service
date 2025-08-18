8000a7eb6363727bb522010ab9637448b044fc34
Kush Gupta <Kushg827@gmail.com>
2025-08-18 18:52:24 +0530

    Added roles and user_roles through table

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const { PORT } = require("./config/serverConfig.js");
 const apiRoutes = require('./routes/index.js');
+// const db = require('./models/index');
+// const {User, Role} = require('./models/index.js');
 const app = express();
 
 const prepareAndStartServer = () => {
@@ -8,8 +10,15 @@ const prepareAndStartServer = () => {
     app.use(express.urlencoded({ extended: true }));
     app.use('/api', apiRoutes);
 
-    app.listen(PORT, () => {
+    app.listen(PORT, async () => {
         console.log(`Server is running on port ${PORT}`);
+        // if(process.env.DB_SYNC) {
+        //     db.sequelize.sync({alter: true});
+        // }
+
+        // const u1 = await User.findByPk(2);
+        // const r1 = await Role.findByPk(3);
+        // u1.addRole(r1);
     });
 }
 
