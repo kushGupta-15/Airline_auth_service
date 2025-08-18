31f622c3b58bdd43882adb4b4b3bc365a668ad24
Kush Gupta <Kushg827@gmail.com>
2025-08-18 09:19:27 +0530

    Added token generation method

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -1,27 +1,52 @@
-const UserService = require('../services/user-service.js')
+const UserService = require("../services/user-service.js");
 
 const userService = new UserService();
 
 const create = async (req, res) => {
+  try {
+    const response = await userService.create(req.body);
+    return res.status(201).json({
+      message: "User created successfully",
+      data: response,
+      success: true,
+      err: {},
+    });
+  } catch (error) {
+    console.log("Error in create in user controller");
+    return res.status(500).json({
+      message: "Something went wrong",
+      data: {},
+      success: false,
+      err: error,
+    });
+  }
+};
+
+// Error in signIn controller need to fix
+const signIn = async (req, res) => {
     try {
-        const response = await userService.create(req.body);
-        return res.status(201).json({
-            message: "User created successfully",
+        const response = await userService.signIn(
+          req.body.email,
+          req.body.password
+        );
+        return res.status(200).json({
+            message: "User signed in successfully",
             data: response,
             success: true,
-            err: {}
+            err: null
         })
     } catch (error) {
-        console.log("Error in create in user controller");
+        console.log("Error in signIn in user controller");
         return res.status(500).json({
-          message: "Something went wrong",
-          data:{},
-          success: false,
-          err: error
+            message: "Something went wrong",
+            data:{},
+            success: false,
+            err: error
         });
     }
 }
 
 module.exports = {
-    create
-}
+  create,
+  signIn,
+};
--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,50 +1,68 @@
-const UserRepository = require('../repository/user-repository.js')
-const {JWT_KEY} = require('../config/serverConfig.js');
+const UserRepository = require("../repository/user-repository.js");
+const { JWT_KEY } = require("../config/serverConfig.js");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 
 class UserService {
-    constructor() {
-        this.userRepository = new UserRepository();
+  constructor() {
+    this.userRepository = new UserRepository();
+  }
+
+  async create(data) {
+    try {
+      const user = await this.userRepository.create(data);
+      return user;
+    } catch (error) {
+      console.log("Error in create in user service");
+      throw error;
+    }
+  }
+
+    async signIn(email, plainPassword) {
+      try {
+        const user = await this.userRepository.getByEmail(email);
+        const passwordMatch = this.checkPassword(plainPassword, user.password);
+        if(!passwordMatch) {
+            console.log("Password doesn't match");
+            throw {error: 'Incorrect Password'};
+        }
+        const newJWT = this.createToken({email: user.email, id:user.id});
+        return newJWT;
+        
+      } catch (error) {
+        console.log("Error in signIn in user service");
+        throw error;
+      }
     }
 
-    async create(data) {
-        try {
-            const user = await this.userRepository.create(data);
-            return user;
-        } catch (error) {
-            console.log("Error in create in user service");
-            throw error;
-        }
+  createToken(user) {
+    try {
+      const result = jwt.sign(user, JWT_KEY, { expiresIn: "1h" });
+      return result;
+    } catch (error) {
+      console.log("Error in create token in token creation");
+      throw error;
     }
+  }
 
-    async getById(userId) {
-        try {
-            const user = await this.userRepository.getById(userId);
-            return user;
-        } catch (error) {
-            console.log("Error in getById in user service");
-            throw error;
-        }
+  verifyToken(token) {
+    try {
+      const response = jwt.verify(token, JWT_KEY);
+      return response;
+    } catch (error) {
+      console.log("Error in verifying token in user service");
+      throw error;
     }
+  }
 
-    createToken(user) {
-        try {
-            const result = JWT_KEY.sign(user, JWT_KEY, {expiresIn: '1h'});
-            return result;
-        } catch (error) {
-            console.log("Error in create token in token creation");
-            throw error;
-        }
+  checkPassword(userInputPlainPassword, encryptedPassword) {
+    try {
+      return bcrypt.compareSync(userInputPlainPassword, encryptedPassword);
+    } catch (error) {
+      console.log("Error in checking password in user service");
+      throw error;
     }
-
-    verifyToken(token) {
-        try {
-            const response = jwt.verify(token, JWT_KEY);
-            return response;
-        } catch (error) {
-            console.log("Error in verigying token in user service");
-            throw error;
-        }
-    }
+  }
 }
 
 module.exports = UserService;
